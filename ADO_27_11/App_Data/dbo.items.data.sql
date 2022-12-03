7b034570af9c67f13bd66cf81e97a25a73edfbd9
Amit Palti <amit.palti@campus.technion.ac.il>
2022-12-03 20:42:41 +0200

    adding

---
+++ ADO_27_11/App_Data/dbo.items.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[items] ON
+INSERT INTO [dbo].[items] ([Id], [ItemName], [Price], [DateInStore]) VALUES (2, N'Hat', CAST(44.0000 AS Money), N'2002-02-02')
+INSERT INTO [dbo].[items] ([Id], [ItemName], [Price], [DateInStore]) VALUES (3, N'Shirt', CAST(33.0000 AS Money), N'2002-01-01')
+INSERT INTO [dbo].[items] ([Id], [ItemName], [Price], [DateInStore]) VALUES (4, N'Socks', CAST(1111.0000 AS Money), N'2005-05-05')
+SET IDENTITY_INSERT [dbo].[items] OFF
---
+++ ADO_27_11/App_Data/dbo.items.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[items] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [ItemName]    NVARCHAR (50) NOT NULL,
+    [Price]       MONEY         NOT NULL,
+    [DateInStore] DATE          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
